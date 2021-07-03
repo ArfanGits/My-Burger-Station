cc2da3b048731841060461087c9858e68fa8fa2b
Arfan A. Khan <73901113+ArfanGits@users.noreply.github.com>
2021-07-03 21:02:05 +0600

    Using Redux for Authenticaiton

--- src/redux/authActionCreators.js
+++ src/redux/authActionCreators.js
@@ -1,5 +1,15 @@
+import * as actionTypes from './actionTypes';
 import axios from 'axios';
 
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        payload: {
+            token: token,
+            userId: userId,
+        }
+    }
+}
 
 export const auth = (email, password, mode) => dispatch => {
     const authData = {
@@ -16,5 +26,11 @@ export const auth = (email, password, mode) => dispatch => {
     }
     const API_KEY = "AIzaSyA4nq_JgYXTcrdRVz0kOnLtZDce_45J414";
     axios.post(authUrl + API_KEY, authData)
-        .then(response => console.log(response))
+        .then(response => {
+            localStorage.setItem('token', response.data.idToken);
+            localStorage.setItem('userId', response.data.localId);
+            const expirationTime = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+            localStorage.setItem('expirationTime', expirationTime);
+            dispatch(authSuccess(response.data.idToken, response.data.localId));
+        })
 }
