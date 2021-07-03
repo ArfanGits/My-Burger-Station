5fe25a3e0c84f80228e0c4c1461d8a461f137ce9
Arfan A. Khan <73901113+ArfanGits@users.noreply.github.com>
2021-07-03 10:46:32 +0600

    Switching between Sign up and Login

--- src/Components/Orders/Checkout/Checkout.js
+++ src/Components/Orders/Checkout/Checkout.js
@@ -7,8 +7,6 @@ import axios from 'axios';
 import { connect } from 'react-redux';
 import { resetIngredients } from '../../../redux/actionCreators';
 
-import { Formik } from 'formik';
-
 const mapStateToProps = state => {
     return {
         ingredients: state.ingredients,
@@ -25,6 +23,11 @@ const mapDispatchToProps = dispatch => {
 
 class Checkout extends Component {
     state = {
+        values: {
+            deliveryAddress: "",
+            phone: "",
+            paymentType: "Cash On Delivery",
+        },
         isLoading: false,
         isModalOpen: false,
         modalMsg: "",
@@ -34,11 +37,20 @@ class Checkout extends Component {
         this.props.history.goBack("/");
     }
 
-    submitHandler = (values) => {
+    inputChangerHandler = (e) => {
+        this.setState({
+            values: {
+                ...this.state.values,
+                [e.target.name]: e.target.value,
+            }
+        })
+    }
+
+    submitHandler = () => {
         this.setState({ isLoading: true });
         const order = {
             ingredients: this.props.ingredients,
-            customer: values,
+            customer: this.state.values,
             price: this.props.totalPrice,
             orderTime: new Date(),
         }
@@ -76,58 +88,25 @@ class Checkout extends Component {
                 borderRadius: "5px",
                 padding: "20px",
             }}>Payment: {this.props.totalPrice} BDT</h4>
-            <Formik
-                initialValues={{
-                    deliveryAddress: "",
-                    phone: "",
-                    paymentType: "Cash On Delivery",
-                }}
-                validate={values => {
-                    const errors = {};
-                    return errors;
-                }}
-                onSubmit={(values) => {
-                    this.submitHandler(values);
-                }}
-            >
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    /* and other goodies */
-                }) => (
-                    < form style={{
-                        border: "1px solid grey",
-                        boxShadow: "1px 1px #888888",
-                        borderRadius: "5px",
-                        padding: "20px",
-                    }} onSubmit={handleSubmit}>
-                        <textarea
-                            name="deliveryAddress"
-                            id="deliveryAddress"
-                            value={values.deliveryAddress}
-                            className="form-control"
-                            placeholder="Your Address"
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                        ></textarea>
-                        <span>{errors.deliveryAddress && touched.deliveryAddress && errors.deliveryAddress}</span>
-                        <br />
-                        <input name="phone" id="phone" className="form-control" value={values.phone} placeholder="Your Phone Number" onBlur={handleBlur} onChange={handleChange} />
-                        <br />
-                        <select name="paymentType" id="paymentType" className="form-control" value={values.paymentType} onBlur={handleBlur} onChange={handleChange}>
-                            <option value="Cash On Delivery">Cash On Delivery</option>
-                            <option value="Bkash">Bkash</option>
-                        </select>
-                        <br />
-                        <Button type="submit" style={{ backgroundColor: "#D70F64" }} className="mr-auto" disabled={!this.props.purchasable}>Place Order</Button>
-                        <Button color="secondary" className="ml-1" onClick={this.goBack}>Cancel</Button>
-                    </form>)}
-            </Formik>
-        </div >)
+            <form style={{
+                border: "1px solid grey",
+                boxShadow: "1px 1px #888888",
+                borderRadius: "5px",
+                padding: "20px",
+            }}>
+                <textarea name="deliveryAddress" value={this.state.values.deliveryAddress} className="form-control" placeholder="Your Address" onChange={(e) => this.inputChangerHandler(e)}></textarea>
+                <br />
+                <input name="phone" className="form-control" value={this.state.values.phone} placeholder="Your Phone Number" onChange={(e) => this.inputChangerHandler(e)} />
+                <br />
+                <select name="paymentType" className="form-control" value={this.state.values.paymentType} onChange={(e) => this.inputChangerHandler(e)}>
+                    <option value="Cash On Delivery">Cash On Delivery</option>
+                    <option value="Bkash">Bkash</option>
+                </select>
+                <br />
+                <Button style={{ backgroundColor: "#D70F64" }} className="mr-auto" onClick={this.submitHandler} disabled={!this.props.purchasable}>Place Order</Button>
+                <Button color="secondary" className="ml-1" onClick={this.goBack}>Cancel</Button>
+            </form>
+        </div>)
         return (
             <div>
                 {this.state.isLoading ? <Spinner /> : form}
@@ -142,4 +121,3 @@ class Checkout extends Component {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
-
